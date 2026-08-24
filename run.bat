1664cabff573226b5de06f3f05afb63059d5858f
Alana Alexia <alanaalexia.h@gmail.com>
2026-08-24 10:55:19 -0300

    feat(jupyter): screen invertion

--- run.bat
+++ run.bat
@@ -8,11 +8,11 @@ if not exist venv (
 )
 
 :: 2. Activate the environment (if not activated)
-call .venv\Cripts\activate.bat
+call .venv\Scripts\activate.bat
 
 :: 3. Install dependencies
 echo Installing the dependencies from requirements.txt...
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 :: 4. Register Jupyter kernel
 python -m ipykernel install --user --name=.venv --display-name "prototype.ipynb"
